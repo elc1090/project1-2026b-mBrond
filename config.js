6a4ba6359ea1fe81b6eed0ffd33e5526f8755d7f
Miguel Brondani <brondani.miguel@gmail.com>
2026-08-24 23:17:51 -0300

    fix: GAS url

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwhQIjDc7IqU09LTOs8eu08UwJ5qLuO9exjMXtCBw_9HvBDmaQPuTaHj2zYZhqnbo-vrA/exec";
+export const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwCADZAsmNCIJhT56vhygw9W792N4OMRbu9M89FAeIBHJ0GlWRkeEGQW82odSsmxe6d7A/exec";
 export const FRONTEND_VERSION = "1.1.0";
